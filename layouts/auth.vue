4bf5a01fccc3ea911fbee4f6881a6c49b80ab746
AiDinaAgustin <aidinaagustin2@gmail.com>
2025-09-07 14:39:39 +0700

    add auth and protected components with layout structure and slot support

---
+++ layouts/auth.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div
+      class="flex items-center justify-center min-h-screen sm:p-6 lg:p-8 p-4"
+  >
+    <section
+        class="max-w-lg w-full p-6 border border-gray-200 rounded-lg shadow-sm"
+    >
+      <slot />
+    </section>
+  </div>
+</template>
---
+++ layouts/protected.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Navbar from "/components/Navbar.vue";
+import Footer from "/components/Footer.vue";
+</script>
+
+<template>
+  <div>
+    <header>
+      <slot name="header">
+        <Navbar />
+      </slot>
+    </header>
+    <main>
+      <slot />
+    </main>
+    <footer>
+      <slot name="footer">
+        <Footer />
+      </slot>
+    </footer>
+  </div>
+</template>
