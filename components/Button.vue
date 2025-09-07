39e4ba79c583ebe5f6babe9d35c939a8debf2e63
AiDinaAgustin <aidinaagustin2@gmail.com>
2025-09-07 14:39:19 +0700

    add Button, Input, Label, Footer, and Navbar components with Flowbite integration

---
+++ components/Button.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({});
+</script>
+
+<template>
+  <button
+      v-bind="$attrs"
+      class="font-medium rounded-lg text-sm px-5 py-2.5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+  >
+    <slot />
+  </button>
+</template>
